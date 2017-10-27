f3ec7747b0dafcf34929784595217032f0711cfe
Katy <komal.vadukul@gmail.com>
2017-10-27 03:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key]= value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var copy = Object.assign({}, object);
+  delete object[key];
+  return Object.assign({}, object, {[key]:value});
+}
