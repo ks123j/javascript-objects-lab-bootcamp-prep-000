43e0588653e8d86c4eb52986e42568e3c7b01380
Katy <komal.vadukul@gmail.com>
2017-10-27 03:51:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {key:'value'};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value;
+  return object;
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value;
+  return object;
+}
