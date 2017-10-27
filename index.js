80e9aac93c198b8a4a5678ccbc03162532b1dac0
Katy <komal.vadukul@gmail.com>
2017-10-27 04:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object);
+  var newObject = Object.assign(object);
   delete newObject[key];
   return newObject;
 }
